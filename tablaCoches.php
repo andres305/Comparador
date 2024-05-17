f1cd2db7597ef5a9499b47c525c48f3cfdece46d
Andres <andresjimber@gmail.com>
2024-05-17 13:25:10 +0200

    e

--- tablaCoches.php
+++ tablaCoches.php
@@ -51,7 +51,17 @@ class Coche {
     }
 }
 
-include_once "base_de_datos.php";
+$contraseña = "";
+$usuario = "root";
+$nombre_base_de_datos = "comparador";
+
+try {
+    // Intentamos crear una instancia de PDO para la conexión a la base de datos
+    $base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+} catch (Exception $e) {
+    // Si ocurre un error, mostramos un mensaje
+    echo "Ocurrió algo con la base de datos: " . $e->getMessage();
+}
 
 // Utilizamos el método getAll de la clase Coche para obtener todos los coches
 $coches = Coche::getAll($base_de_datos);
