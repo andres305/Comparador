8ffe615719f6f8089fcc82fdc03662792c7fedb1
Andres <andresjimber@gmail.com>
2024-05-22 13:42:40 +0200

    cambio estilos

--- tablaCoches.php
+++ tablaCoches.php
@@ -27,16 +27,18 @@ $coches = Coche::getAll($base_de_datos);
     foreach($coches as $coche) { ?>
         <article>
             <img src="<?php echo $coche->rutaImagen; ?>"><br>
-            Marca: <?php echo $coche->marca; ?><br>
-            Modelo: <?php echo $coche->modelo; ?><br>
-            País: <?php echo $coche->pais; ?><br>
-            Precio: <?php echo $coche->precio; ?>€<br>
-            Caballos: <?php echo $coche->caballos; ?>cv<br>
-            Maletero: <?php echo $coche->maletero; ?>L<br>
-            Puertas: <?php echo $coche->puertas; ?><br>
+            <div id="txt">
+            <b>Marca:</b> <?php echo $coche->marca; ?><br>
+            <b>Modelo:</b> <?php echo $coche->modelo; ?><br>
+            <b>País:</b> <?php echo $coche->pais; ?><br>
+            <b>Precio:</b> <?php echo $coche->precio; ?>€<br>
+            <b>Caballos:</b> <?php echo $coche->caballos; ?>cv<br>
+            <b>Maletero:</b> <?php echo $coche->maletero; ?>L<br>
+            <b>Puertas:</b> <?php echo $coche->puertas; ?><br>
+            </div>
         </article>
     <?php } ?>
     </div>
-    <p><a href="LogIn/logout.php">Cerrar sesión</a></p>
+    <a href="LogIn/logout.php"><button id="btnLogout">Cerrar sesión</button></a>
 </body>
 </html>
